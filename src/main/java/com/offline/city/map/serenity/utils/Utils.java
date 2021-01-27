e23af6a1b8487cdea5ecda95884a11357842204e
Daniel Kolodzig <daniel.kolodzig@mobimeo.com>
2021-01-27 22:47:14 +0100

    remove unnecessary code

--- src/main/java/com/offline/city/map/serenity/utils/Utils.java
+++ src/main/java/com/offline/city/map/serenity/utils/Utils.java
@@ -30,36 +30,4 @@ public class Utils {
         deviceType = Serenity.sessionVariableCalled("deviceType");
         return deviceType.equalsIgnoreCase("android");
     }
-
-    public static Map<String, List<String>> convertListOfListsToMapOfLists(List<List<String>> listOflist){
-
-        Map<String, List<String>> mapOfLists = new HashMap<String, List<String>>();
-
-        mapOfLists.put(listOflist.get(0).get(0), listOflist.get(0).subList(1, listOflist.get(0).size()));
-        mapOfLists.put(listOflist.get(1).get(0), listOflist.get(1).subList(1, listOflist.get(0).size()));
-        mapOfLists.put(listOflist.get(2).get(0), listOflist.get(2).subList(1, listOflist.get(0).size()));
-        mapOfLists.put(listOflist.get(3).get(0), listOflist.get(3).subList(1, listOflist.get(0).size()));
-        return mapOfLists;
-    }
-
-    public static void scrollUpFromElementToPixel(WebElement webElementFacade, int verticalScrollingPixelAmount, int waitingTime) {
-
-        //Serenity.getWebdriverManager().getWebdriver()
-
-        MobileDriver mobileDriver = (MobileDriver)((WebDriverFacade) Serenity.getWebdriverManager().getWebdriver()).getProxiedDriver();
-
-        int startX = webElementFacade.getLocation().getX() + (webElementFacade.getSize().getWidth() / 2);
-        int startY = webElementFacade.getLocation().getY() + (webElementFacade.getSize().getHeight() / 2);
-
-        //since its a vertical scroll, thePrivacyPolicy endX position is thePrivacyPolicy same as startX, only endY controls thePrivacyPolicy swiping distance:
-        int endX = startX;
-        int endY = startY - verticalScrollingPixelAmount;
-
-        TouchAction scrollUpToSelectAnotherDay = new TouchAction(mobileDriver)
-                .press(point(startX,startY))
-                .waitAction(waitOptions(ofMillis(waitingTime)))
-                .moveTo(point(endX, endY))
-                .release();
-        scrollUpToSelectAnotherDay.perform();
-    }
 }
